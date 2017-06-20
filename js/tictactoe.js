4ef0c30173c61d083eae89488e2415e833c68072
g p <giulio.pietroiusti@gmail.com>
2017-06-20 16:18:41 +0200

    Insert sign only if square is void and chosen is set

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,6 +1,9 @@
 window.onload = function() {
+    // buttons for chose 'O' or 'X'
     var choiceBtns = document.querySelectorAll('.choicebtn');
+    // squares of the game
     var gameBtns = document.querySelectorAll('.gamebtn');
+    // store sign chosen by user ('O' or 'X')
     var chosen = null;
   
     for (let i = 0; i < choiceBtns.length; i++) {
@@ -10,16 +13,21 @@ window.onload = function() {
     for (let i = 0; i < gameBtns.length; i++) {
 	gameBtns[i].addEventListener('click', markSquare);
     }
-
+    
     function setChosen() {
 	if (chosen == null)
 	    chosen = this.firstChild.nodeValue;
     }
 
+    // insert sign into square
     function markSquare() {
-	var mark = document.createElement('span');
-	var markText = document.createTextNode(chosen);
-	mark.appendChild(markText);
-	this.appendChild(mark);
+	if (chosen !== null) {
+	    if (this.childNodes.length === 0) { //If the square hasn't been marked already
+		var mark = document.createElement('span');
+		var markText = document.createTextNode(chosen);
+		mark.appendChild(markText);
+		this.appendChild(mark);
+	    }
+	}
     }
 };

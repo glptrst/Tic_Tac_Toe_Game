b95c51977b2c2df5526623a7265322d0d08158d8
g p <giulio.pietroiusti@gmail.com>
2017-06-21 14:52:00 +0200

    Allow restarting game without refreshing page

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -3,31 +3,59 @@ window.onload = function() {
     var choiceBtns = document.querySelectorAll('.choicebtn');
     // squares of the game
     var gameBtns = document.querySelectorAll('.gamebtn');
-    // store sign chosen by user ('O' or 'X')
-    var chosen = null;
+    // store user and computer sign ('O' or 'X')
+    var userSign = null;
+    var computerSign = null;
+    
+    document.getElementById('restart').addEventListener('click', restart);
   
     for (let i = 0; i < choiceBtns.length; i++) {
-	choiceBtns[i].addEventListener('click', setChosen);
+	choiceBtns[i].addEventListener('click', setUserSign);
     }
 
     for (let i = 0; i < gameBtns.length; i++) {
 	gameBtns[i].addEventListener('click', markSquare);
     }
     
-    function setChosen() {
-	if (chosen == null)
-	    chosen = this.firstChild.nodeValue;
+    function setUserSign() {
+	if (userSign == null) {
+	    userSign = this.firstChild.nodeValue;
+	    if (userSign === 'X')
+		computerSign = 'O';
+	    else
+		computerSign = 'X';
+	}
     }
 
     // insert sign into square
     function markSquare() {
-	if (chosen !== null) {
+	if (userSign !== null) {
 	    if (this.childNodes.length === 0) { //If the square hasn't been marked already
 		var mark = document.createElement('span');
-		var markText = document.createTextNode(chosen);
+		var markText = document.createTextNode(userSign);
 		mark.appendChild(markText);
 		this.appendChild(mark);
 	    }
 	}
     }
+
+    function restart() {
+	userSign = null;
+	for (let i = 0; i < gameBtns.length; i++) {
+	    if (gameBtns[i].firstChild !== null)
+		gameBtns[i].firstChild.remove();
+	}
+    }
+
+    function computerPlay() {
+	// select a random square
+	var squareId = randomRange(1, 9);
+
+    }
+
+    // generate a random number between a range
+    function randomRange(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
 };
